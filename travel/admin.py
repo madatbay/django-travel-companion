afa98dcd26ea89c9f3f301074213bbbe7b27d957
Madat Bay <medet.bayramov22@gmail.com>
2022-02-24 22:38:50 +0400

    trip model created

--- travel/admin.py
+++ travel/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Trip
 
-# Register your models here.
+
+@admin.register(Trip)
+class TripAdmin(admin.ModelAdmin):
+    list_display = ["name", "user", "start_date", "end_date", "created_at"]
+    list_filter = ["user", "start_date", "end_date", "created_at"]
+    search_fields = ["name", "user", "start_date", "end_date", "created_at"]
+    filter_horizontal = ["trip_mates"]
+
+    fieldsets = (
+        (
+            "General Information",
+            {
+                "fields": ("user", "name", "description"),
+            },
+        ),
+        (
+            "Options",
+            {
+                "fields": ("trip_mates",),
+            },
+        ),
+        (
+            "Schedule",
+            {
+                "fields": ("start_date", "end_date"),
+            },
+        ),
+    )
--- travel/models.py
+++ travel/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Trip(models.Model):
+    user = models.ForeignKey("user.User", related_name="user", on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=200, help_text="Short trip description")
+    trip_mates = models.ManyToManyField(
+        "user.User", related_name="mates", help_text="Choose who is going with you", blank=True
+    )
+    start_date = models.DateField(help_text="Trip start date")
+    end_date = models.DateField(help_text="Trip end date")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_at"]
+
+    def __str__(self) -> str:
+        return self.name
+
+    def __repr__(self) -> str:
+        return f"<Trip: {self.name}x{self.pk}>"
