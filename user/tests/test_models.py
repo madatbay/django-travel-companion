4a1cba5f402c6715a07899a5ea9e0114ce17055c
Madat Bay <medet.bayramov22@gmail.com>
2022-02-28 00:53:03 +0400

    returns removed

--- user/tests/test_models.py
+++ user/tests/test_models.py
@@ -11,25 +11,25 @@ class TestUserModel(TestCase):
         )
 
     def test_get_avatar(self):
-        return self.assertEqual(self.testuser2.get_avatar, "/media/avatar.png")
+        self.assertEqual(self.testuser2.get_avatar, "/media/avatar.png")
 
     def test_get_avatar_no_avatar(self):
-        return self.assertEqual(self.testuser.get_avatar, "/static/user/img/default.png")
+        self.assertEqual(self.testuser.get_avatar, "/static/user/img/default.png")
 
     def test_avatar_preview(self):
-        return self.assertEqual(self.testuser2.avatar_preview, '<img src="/media/avatar.png" width="50" height="50" />')
+        self.assertEqual(self.testuser2.avatar_preview, '<img src="/media/avatar.png" width="50" height="50" />')
 
     def test_avatar_preview_no_avatar(self):
-        return self.assertEqual(self.testuser.avatar_preview, "Not set")
+        self.assertEqual(self.testuser.avatar_preview, "Not set")
 
     def test_instance_str(self):
-        return self.assertEqual(self.testuser.__repr__(), "[test@test.com]")
+        self.assertEqual(self.testuser.__repr__(), "[test@test.com]")
 
     def test_instance_repr(self):
-        return self.assertEqual(self.testuser.__str__(), "test@test.com")
+        self.assertEqual(self.testuser.__str__(), "test@test.com")
 
     def test_has_perm(self):
-        return self.assertEqual(self.testuser.has_perm("test_perm"), True)
+        self.assertEqual(self.testuser.has_perm("test_perm"), True)
 
     def test_has_module_perm(self):
-        return self.assertEqual(self.testuser.has_module_perms("test_app"), True)
+        self.assertEqual(self.testuser.has_module_perms("test_app"), True)
