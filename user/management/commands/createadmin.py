e7cf9a0079c6160ed5e6725009e3332b755337bc
Madat Bay <medet.bayramov22@gmail.com>
2022-03-01 01:07:49 +0400

    custom django command added

---
+++ user/management/commands/createadmin.py
@@ -0,0 +1,27 @@
+import secrets
+import string
+
+from django.core.management.base import BaseCommand
+from user.models import User
+
+
+def generate_password(length: int):
+    alphabet = string.ascii_letters + string.digits
+    return "".join(secrets.choice(alphabet) for i in range(length))
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        if User.objects.count() == 0:
+            email = "root@admin.com"
+            password = generate_password(15)
+            admin = User.objects.create_superuser(email=email, password=password)
+            admin.is_active = True
+            admin.is_admin = True
+            admin.save()
+            print(
+                f"_________________________________________________ \n"
+                f"Initial ADMIN ACCOUNT email={email}, password={password}. \n"
+                f"DO CHANGE THE PASSWORD AFTER FIRST LOGIN \n"
+                f"_________________________________________________ \n"
+            )
